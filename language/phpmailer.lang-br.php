014666f5b66e35dc3f3d462d479a76c70de3f458
FlorianLANNUZEL <florian@lannuzel.bzh>
2018-04-09 17:25:14 +0200

    Language: br_FR
    Breton translation / Troidigezh vrezhoneg / Traduction en langue bretonne

---
+++ language/phpmailer.lang-br.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Breton PHPMailer language file: refer to English translation for definitive list
+ * @package PHPMailer
+ * @author @FlorianLANNUZEL <florian@lannuzel.bzh>
+ */
+
+$PHPMAILER_LANG['authenticate']         = 'Fazi dafariad kas (SMTP): c’hwitadur an dilesa.';
+$PHPMAILER_LANG['connect_host']         = 'Fazi dafariad kas (SMTP): n’hall ket en em zilesa ouzh an dafariad.';
+$PHPMAILER_LANG['data_not_accepted']    = 'Fazi dafariad kas (SMTP): roadennoù didalvoudek.';
+$PHPMAILER_LANG['empty_message']        = 'Kemennadenn c’houllo.';
+$PHPMAILER_LANG['encoding']             = 'Bonegañ dianav: ';
+$PHPMAILER_LANG['execute']              = 'N’haller ket lañsañ an erounezadur: ';
+$PHPMAILER_LANG['file_access']          = 'N’haller ket haeziñ ouzh ar restr: ';
+$PHPMAILER_LANG['file_open']            = 'N’haller ket digeriñ ar restr: ';
+$PHPMAILER_LANG['from_failed']          = 'C’hwitadenn war chomlec’h postel ar c’haser-mañ: ';
+$PHPMAILER_LANG['instantiate']          = 'N’haller ket esteziañ an arc’hwel postel.';
+$PHPMAILER_LANG['invalid_address']      = 'Ar chomlec’h postel n’eo ket talvoudek: ';
+$PHPMAILER_LANG['mailer_not_supported'] = ' posteler anskoret.';
+$PHPMAILER_LANG['provide_address']      = 'Mar plij, bizskrivit chomlec’h postel un degemerer da nebeutañ.';
+$PHPMAILER_LANG['recipients_failed']    = 'Fazi dafariad kas (SMTP): n’eo ket reizh an degemererien da heul: ';
+$PHPMAILER_LANG['signing']              = 'Fazi e-pad an dilesa: ';
+$PHPMAILER_LANG['smtp_connect_failed']  = 'C’hwitadenn war ar c’hennaskañ SMTP.';
+$PHPMAILER_LANG['smtp_error']           = 'Fazi dafariad kas (SMTP): ';
+$PHPMAILER_LANG['variable_set']         = 'N’haller ket despizañ pe adderaouekaat un argemmenn: ';
+$PHPMAILER_LANG['extension_missing']    = 'Askouezh o vankout: ';
